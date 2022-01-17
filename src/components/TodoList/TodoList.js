de91a4fb2e480a990b0114d8394ea2689b66cc68
jhon garcia <jagg499@gmail.com>
2022-01-17 14:14:20 -0500

    better compotition to todoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -5,7 +5,13 @@ const TodoList = (props) =>{
     return(
         <section>
             <ul className={styles.ul}> 
-                {props.children}
+            {props.error && props.onError()}
+            {props.loading && props.onLoading()}
+            {(!props.loading && !props.numberCompletedTodos) && props.onEmptyTodos()}
+            {(props.numberCompletedTodos && !props.searchedTodos.length === 0) && props.onEmptySearch(props.search)}
+            {props.searchedTodos.map(
+                todo => props.render(todo)
+            )}
             </ul>
         </section>
     )
